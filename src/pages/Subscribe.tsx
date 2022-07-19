dcc7544be7cf1702fbccbd21ec9918046f78ea22
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2022-07-19 11:00:16 -0300

    feat: ✨Capture and send value

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,40 @@
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { FormEvent, useState } from "react"
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../assets/logo"
 
+const CREATE_SUBSCRIBE_MUTATION = gql`
+mutation MyMutation ($name: String!, $phone: String, $email: String!){
+  createSubscriber(data: {name: $name, phoneNumber: $phone email: $email}){
+    id
+  }
+}
+
+`
+
 function Subscribe(){
+    const navigate = useNavigate();
+    const [ name, setName ] = useState('');
+    const [ phone, setPhone ] = useState('');
+    const [ email, setEmail ] = useState('');
+
+    const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBE_MUTATION)
+
+    async function handleSubscribe(event: FormEvent){
+        event.preventDefault();
+
+        await createSubscriber({
+            variables: {
+                name,
+                phone,
+                email
+            }
+        }).then(() => navigate('/course')).catch(()=> navigate('/course'))
+
+        
+
+    }
+
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
             <div className="w-full max-w-[1300px] px-10 flex items-center justify-between mt-20 mx-auto flex-col gap-5 lg:flex-row">
@@ -21,28 +55,32 @@ function Subscribe(){
                 <div className="p-8 bg-gray-700 border border-gray-500 rounded max-w-[640px] w-full lg:w-auto">
                     <h2 className="text-2xl mb-6 black font-bold">Inscreva-se gratuitamente</h2>
 
-                    <form action="" className="flex flex-col gap-2 w-full">
+                    <form action="" className="flex flex-col gap-2 w-full" onSubmit={handleSubscribe}>
                         <input 
                         className="bg-gray-900 rounded px-5 h-14"
                         type="text" 
                         placeholder="Seu nome completo"
+                        onChange={event => setName(event.target.value)}
                         required
                         />
                         <input 
                         className="bg-gray-900 rounded px-5 h-14"
                         type="tel"
                         placeholder="Digite seu telefone" 
+                        onChange={event => setPhone(event.target.value)}
                         required
                         />
                         <input 
                         className="bg-gray-900 rounded px-5 h-14"
                         type="email"
                         placeholder="Digite seu e-mail" 
+                        onChange={event => setEmail(event.target.value)}
                         required
                         />
 
                         <button 
-                        className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                        className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+                        disabled={loading}
                         type="submit"
                         >
                             Garantir vaga
